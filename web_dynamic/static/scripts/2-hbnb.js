935039ff0e8883c40861d1ebd6e69b121c02ec1f
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-18 12:10:35 -0300

    Update JS to retrive api status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,21 +1,23 @@
-$(document).ready(function() {
-  const amenityDict = {};
-  $('input[type="checkbox"]').click(function () {
-    if ($(this).is(':checked')) {
-      amenityDict[$(this).data('id')] = $(this).data('name');
-      $('div.amenities h4').text(Object.values(amenityDict).join(', '));
-    } else if ($(this).is(':not(:checked)')) {
-      delete amenityDict[$(this).data('id')];
-      $('div.amenities h4').text(Object.values(amenityDict).join(', '));
-    }
-    console.log(amenityDict);
-  });
-  $.get('http://localhost:5001/api/v1/status/', function (data) {
-    alert(data.status)
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+$(document).ready(function () {
+  const amenityId = {};
+  $('input[type=checkbox]').change(function () {
+    if ($(this).prop('checked')) {
+      amenityId[$(this).attr('data-id')] = $(this).attr('data-name');
     } else {
-      $('#api_status').removeClass('available');
+      delete amenityId[$(this).attr('data-id')];
+    }
+    if (amenityId.length === 0) {
+      $('div.amenities h4').html('&nbsp;');
+    } else {
+      $('div.amenities h4').text(Object.values(amenityId).join(', '));
     }
   });
+});
+$.get('http://localhost:5001/api/v1/status/', function (data) {
+  alert(data.status)
+  if (data.status === 'OK') {
+    $('#api_status').addClass('available');
+  } else {
+    $('#api_status').removeClass('available');
+  }
 });
