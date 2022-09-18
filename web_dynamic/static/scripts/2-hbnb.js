8b101d273e201f6cf56b38d5252c582a75f2f5c9
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-18 13:34:23 -0300

    Update URL API

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
     }
   });
 });
-$.get('http://localhost:5001/api/v1/status/', function (data) {
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
   if (data.status === 'OK') {
     $('#api_status').addClass('available');
   } else {
