68429a9c001990e3eecc714f8e4a38a78418399d
SFreire2022 <sebastiancazenave@gmail.com>
2022-09-18 12:15:54 -0300

    Delete debug alert status

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -14,7 +14,6 @@ $(document).ready(function () {
   });
 });
 $.get('http://localhost:5001/api/v1/status/', function (data) {
-  alert(data.status)
   if (data.status === 'OK') {
     $('#api_status').addClass('available');
   } else {
